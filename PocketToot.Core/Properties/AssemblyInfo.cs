cc75d7af1b8fcfab39ab893f79905a1570254fac
Calvin <calvin@cmpct.info>
2018-04-26 18:30:52 -0400

    Update library version info

--- PocketToot.Core/Properties/AssemblyInfo.cs
+++ PocketToot.Core/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PocketToot.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows CE Mastodon client (Core Library)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PocketToot.Core")]
+[assembly: AssemblyCompany("@calvin@cronk.stenoweb.net")]
+[assembly: AssemblyProduct("PocketToot")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
