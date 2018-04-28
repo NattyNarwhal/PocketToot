d75dbe646f5c474136afb94d588cc265fdd3fc9d
Calvin <calvin@cmpct.info>
2018-04-28 19:49:46 -0400

    Cache settings

--- PocketToot/Settings.cs
+++ PocketToot/Settings.cs
@@ -9,14 +9,26 @@ namespace PocketToot
     public static class Settings
     {
         const string KEY = "HKEY_CURRENT_USER\\Software\\PocketToot";
+        static Dictionary<string, string> _settings;
+
+        static Settings()
+        {
+            _settings = new Dictionary<string, string>();
+        }
 
         public static string GetSetting(string name, string fallback)
         {
-            return (string)Registry.GetValue(KEY, name, fallback);
+            if (_settings.ContainsKey(name))
+                return _settings[name];
+            // fetch cache it otherwise
+            var res = (string)Registry.GetValue(KEY, name, fallback);
+            _settings[name] = res;
+            return res;
         }
 
         public static void SetSetting(string name, string value)
         {
+            _settings[name] = value;
             Registry.SetValue(KEY, name, value);
         }
     }
